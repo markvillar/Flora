ab4bf6cef3ed8f0a58d383be13e7ec6fd5c93c64
Mark Villar <markcvillar@gmail.com>
2019-11-06 16:53:52 +0000

    make article non-editable

--- Flora/Views/DetailedView.swift
+++ Flora/Views/DetailedView.swift
@@ -18,6 +18,7 @@ class DetailedView: UIViewController, WKUIDelegate {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isScrollEnabled = false
+        textView.isEditable = false
         return textView
     }()
     
